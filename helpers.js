2062fe1b64391d277f14961a42a5d8c74fd77b07
Michael Sprague <mikesprague@gmail.com>
2019-01-06 01:39:18 -0500

    :bug: Fix Day.js as replacement for Moment.js
    Add plugin for relative time and comments with others available

--- helpers.js
+++ helpers.js
@@ -7,7 +7,24 @@ const fs = require('fs');
 
 // Day.js 2KB immutable date library alternative to Moment.js with the same modern API
 // we need this in our templates to display things like "Posted 5 minutes ago"
-exports.dayjs = require('dayjs');
+const dayjs = require('dayjs');
+const relativeTime = require('dayjs/plugin/relativeTime');
+// const advancedFormat = require('dayjs/plugin/advancedFormat');
+// const isLeapYear = require('dayjs/plugin/relativeTime');
+// const weekOfYear = require('dayjs/plugin/weekOfYear');
+// const isBetween = require('dayjs/plugin/isBetween');
+// const isSameOrAfter = require('dayjs/plugin/isSameOrAfter');
+// const isSameOrBefore = require('dayjs/plugin/isSameOrBefore');
+
+dayjs.extend(relativeTime);
+// dayjs.extend(advancedFormat);
+// dayjs.extend(isLeapYear);
+// dayjs.extend(weekOfYear);
+// dayjs.extend(isBetween);
+// dayjs.extend(isSameOrAfter);
+// dayjs.extend(isSameOrBefore);
+
+exports.moment = dayjs;
 
 // Dump is a handy debugging function we can use to sort of "console.log" our data
 exports.dump = obj => JSON.stringify(obj, null, 2);
