c6cf14f541838096a345a54f3bd882f94f19a473
Michael Sprague <mikesprague@gmail.com>
2019-01-05 23:49:27 -0500

    :recycle: Replace Moment.js dependency with Day.js

--- helpers.js
+++ helpers.js
@@ -5,8 +5,9 @@
 // FS is a built in module to node that let's us read files from the system we're running on
 const fs = require('fs');
 
-// moment.js is a handy library for displaying dates. We need this in our templates to display things like "Posted 5 minutes ago"
-exports.moment = require('moment');
+// Day.js 2KB immutable date library alternative to Moment.js with the same modern API
+// we need this in our templates to display things like "Posted 5 minutes ago"
+exports.dayjs = require('dayjs');
 
 // Dump is a handy debugging function we can use to sort of "console.log" our data
 exports.dump = (obj) => JSON.stringify(obj, null, 2);
